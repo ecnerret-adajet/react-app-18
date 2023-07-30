da3a38866a558a33ec2f7e425312be1f42ad6eee
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-07-30 23:37:21 +0800

    React 18 Practice

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing > for function components
+function Message() {
+    // JSX: Javascript XML
+    const name = "Terrence";
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hellow World</h1>
+
+}
+
+export default Message;
