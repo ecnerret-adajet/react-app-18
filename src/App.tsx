bf6a08aceab3af359abf4d241dc3a1abeedef6d4
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-01 18:31:36 +0800

    Passing Children component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-
-  let items = [
-    "New York",
-    "San Francisco",
-    "Manila",
-    "Tokyo",
-    "Paris",
-    "London",
-  ];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
-
-  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+  return (
+    <div>
+      <Alert>
+        Hello World <span>test</span> hahah
+      </Alert>
+    </div>
+  );
 }
 
 export default App;
