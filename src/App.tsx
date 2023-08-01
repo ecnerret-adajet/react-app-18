7e64a5c7894bb1197148651dd36bc32947e30f32
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-01 18:14:57 +0800

    Passing Funcions via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import Message from "./Message"
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message/></div>
+
+  let items = [
+    "New York",
+    "San Francisco",
+    "Manila",
+    "Tokyo",
+    "Paris",
+    "London",
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
 }
 
 export default App;
