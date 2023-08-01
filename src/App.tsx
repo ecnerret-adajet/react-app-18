cfbc6ffd9077ab125d3a0889bd86e8eca52c8164
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-01 19:13:22 +0800

    Button and alert component exercise 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+
+  const [visible, setVisible] = useState(false);
+
   return (
-    <Button onClick={() => console.log('clicked')}>
+    <>
+    { visible && <Alert onClose={() => setVisible(false)}>This is an alert message</Alert>}
+    
+    <Button onClick={() => { setVisible(true) }}>
       My button
     </Button>
+    </>
   );
 }
 
