c3e18bff33693e4e3bce658c992b42af6b531054
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-05 17:45:16 +0800

    Simplifying updat elogic with immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import Like from "./components/Like";
+import { useState } from "react";
+import produce from 'immer';
 
 function App() {
 
+  const [bugs, setBugs] = useState([
+    {id: 1, title: 'Bug 1', fixed: false},
+    {id: 2, title: 'Bug 2', fixed: true},
+  ]);
+
+  const handleClick = () => {
+    // setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true} : bug))
+    // draft === a proxy object going to apply the object array
+    // drfat === copy of a array // you dont need a map
+    setBugs(produce(draft => {
+      const bug = draft.find(bug => bug.id === 1);
+      if (bug) bug.fixed = true;
+    }))
+  }
 
   return (
     <>
-      <Like onClick={() => console.log('Clicked')} ></Like>
+      {bugs.map(bug => <p key={bug.id}>{bug.title} {bug.fixed ? 'Fixed' : 'New'}</p>)}
+      <button onClick={handleClick}>Click me</button>
     </>
   );
 }
