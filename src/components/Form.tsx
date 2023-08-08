0d4457abba4d3766a9c1a1097e10bae54805aa78
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-08 13:12:31 +0800

    Controlled Components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,15 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 function Form() {
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
 
-    const nameRef = useRef<HTMLInputElement>(null);
-    const ageRef = useRef<HTMLInputElement>(null);
-    const person = { name: '', age: 0 };
-
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        if (nameRef.current !== null)
-            person.name = nameRef.current.value;
-        if (ageRef.current !== null)
-            person.age = parseInt(ageRef.current.value);
-        console.log(person)
-    }
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(person);
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -21,13 +17,29 @@ function Form() {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
