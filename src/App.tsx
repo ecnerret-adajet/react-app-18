22908e707c441b3f3b96bdc1f66bf00eefa9d522
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-08 13:05:28 +0800

    Accessing input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import ExpandableText from './components/ExpandableText'
+import Form from './components/Form'
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias corporis veniam harum enim natus reprehenderit sunt debitis ipsum voluptate, amet eligendi delectus laborum temporibus officiis quasi dolor consequuntur deserunt, facilis vitae possimus quibusdam animi a. Est doloribus in quae quaerat beatae, soluta non, expedita totam quidem molestias id, ad dicta fuga asperiores quos laboriosam saepe fugiat ratione consequuntur sed iste! Id distinctio aliquid laborum esse, vel eos suscipit, accusamus perferendis reprehenderit sequi aliquam soluta illo deserunt veritatis laboriosam consequuntur autem fuga sunt corporis tempora neque accusantium voluptatibus repellendus quisquam? Repudiandae veritatis cumque, impedit architecto praesentium sint perspiciatis. Beatae, repellat dicta.
-      </ExpandableText>
+      <Form />
     </div>
   )
 }
