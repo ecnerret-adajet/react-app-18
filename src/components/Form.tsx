2919ce153dee1f0ec4eeac55d49b21281d848ffe
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-08 13:22:30 +0800

    Managing Forms with React Hook Forms

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,27 +1,20 @@
 import React, { FormEvent, useRef, useState } from "react";
+import {useForm } from 'react-hook-form';
 
 function Form() {
-  const [person, setPerson] = useState({
-    name: "",
-    age: "",
-  });
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
+   const { register, handleSubmit } = useForm();
+
+   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
-          value={person.name}
+          {...register('name')}
           id="name"
           type="text"
           className="form-control"
@@ -32,10 +25,7 @@ function Form() {
           Age
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, age: parseInt(event.target.value) })
-          }
-          value={person.age}
+          {...register('age')}
           id="age"
           type="number"
           className="form-control"
