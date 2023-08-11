43a887ebe83a61fe5bfb6d1f930f36cac5daf25a
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-11 16:49:48 +0800

    Showing a loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
 
@@ -16,12 +17,20 @@ function App() {
     // built in class in modern browser
     const controller = new AbortController();
 
+    setLoading(true);
     axios.get<User[]>(`https://jsonplaceholder.typicode.com/users`, { signal: controller.signal })
-    .then((res) => setUsers(res.data))
+    .then((res) => {
+      setUsers(res.data)
+      setLoading(false);
+    })
     .catch((err) => {
       if (err instanceof CanceledError) return;
       setError(err.message)
-    });
+      setLoading(false);
+    })
+    // .finally(() => { // after the then or catch is executed, run the code once, not working with string mode is on
+    //   setLoading(false);
+    // });
 
     // clean up function for cancelling the request
     return () => controller.abort();
@@ -31,6 +40,7 @@ function App() {
   return (
     <>
       { error &&  <p className="text-danger">{error}</p>}
+      { loading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
