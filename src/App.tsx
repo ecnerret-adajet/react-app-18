4c7b8a0f6d2078298232d9666ee38b7e1b26600e
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-11 15:45:37 +0800

    Fetch with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import ProductList from './components/ProductList';
+import axios from 'axios';
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
 
-  const [category, setCategory] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+
+    axios.get<User[]>(`https://jsonplaceholder.typicode.com/users`)
+    .then(response => {
+      setUsers(response.data)
+    })
+
+  },[]);
 
   return (
-    <div>
-      <select className="form-select" onChange={(event) => setCategory(event.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category}/>
-    </div>
+    <ul>
+      {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
   )
 }
 
