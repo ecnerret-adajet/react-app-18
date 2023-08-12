e3bea63fad9fe9279cfb733faacc83ee98bd8320
Terrence Tejada <terrence.tejada@lfuggoc.net>
2023-08-12 14:08:16 +0800

    Adding new data

--- src/App.tsx
+++ src/App.tsx
@@ -48,10 +48,24 @@ function App() {
     });
   };
 
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 100, name: "Terrence" };
+    setUsers([newUser, ...users]);
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, newUser)
+    .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+    .catch(err => {
+      setError(err.message);
+      setUsers(originalUsers);
+    })
+  }
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {loading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
       <ul className="list-group">
         {users?.map((user) => (
           <li key={user.id} className="list-group-item d-flex justify-content-between">
